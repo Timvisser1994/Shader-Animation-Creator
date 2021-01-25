9618d15e934e80aa82ba29aaff7d1b73a0d674c2
Tim Visser <tim@timvisser.online>
2021-01-25 10:49:46 +0100

    Added a (very) crude implementation for the color pick to PNG workflow

--- src/main/java/online/timvisser/GradientFactory.java
+++ src/main/java/online/timvisser/GradientFactory.java
@@ -4,6 +4,9 @@ import javafx.scene.paint.Color;
 import javafx.scene.paint.CycleMethod;
 import javafx.scene.paint.LinearGradient;
 import javafx.scene.paint.Stop;
+import online.timvisser.models.RGBAxisMapping;
+
+import static javafx.scene.paint.Color.*;
 
 /**
  * Used to build LinearGradients that correspond with the
@@ -12,17 +15,21 @@ import javafx.scene.paint.Stop;
 public class GradientFactory {
 
     public static LinearGradient buildX() {
+        var targetColor = RGBAxisMapping.getXColor();
+
         var stops = new Stop[] {
             new Stop(0.0, new Color(0, 0, 0, 0)),
-            new Stop(1.0, Color.RED)
+            new Stop(1.0, targetColor)
         };
         return new LinearGradient(0, 0, 1, 1, true, CycleMethod.NO_CYCLE, stops);
     }
 
     public static LinearGradient buildY() {
+        var targetColor = RGBAxisMapping.getYColor();
+
         var stops = new Stop[] {
             new Stop(0.0, new Color(0, 0, 0, 0)),
-            new Stop(1.0, Color.GREEN)
+            new Stop(1.0, targetColor)
         };
         return new LinearGradient(0, 1, 1, 0, true, CycleMethod.NO_CYCLE, stops);
     }
